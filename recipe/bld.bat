28b6d4642b31100e6ceb9cef70f13c03918c2b8d
Carlo <carlo.dri@elettra.eu>
2019-03-11 09:29:10 +0100

    cleanup repo

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,3 @@
-copy "%RECIPE_DIR%\LICENSE" "%SRC_DIR%\LICENSE"
 mkdir "%PREFIX%"\lib
 copy "%SRC_DIR%\%PKG_NAME%-%PKG_VERSION%.jar" "%PREFIX%\lib\bfg.jar"
 copy "%RECIPE_DIR%\bfg.bat" "%SCRIPTS%\bfg.bat"
