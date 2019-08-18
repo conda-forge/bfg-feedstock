76ab6d71548aac7cda47a17fc6103d7d59c4e8f3
Carlo <carlo.dri@gmail.com>
2019-08-19 01:17:59 +0200

    create bin folder on *nix

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-mkdir $PREFIX/lib
+mkdir -p $PREFIX/lib
+mkdir -p $PREFIX/bin
+cp $SRC_DIR/$PKG_NAME-$PKG_VERSION.jar $PREFIX/lib/bfg.jar
 echo "#!/bin/sh" >> $PREFIX/bin/bfg
 echo "java -jar ${PREFIX}/lib/bfg.jar \"$@\"" >> $PREFIX/bin/bfg
-cp $SRC_DIR/$PKG_NAME-$PKG_VERSION.jar $PREFIX/lib/bfg.jar
